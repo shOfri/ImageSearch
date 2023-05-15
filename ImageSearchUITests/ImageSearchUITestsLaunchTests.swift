22e60e81e1a1086c38fa4f2ae27f52f68f3408ba
Ofri Shadmi <ofri@cherryimaging.com>
2023-05-15 14:14:08 +0300

    Initial Commit

---
+++ ImageSearchUITests/ImageSearchUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ImageSearchUITestsLaunchTests.swift
+//  ImageSearchUITests
+//
+//  Created by Ofri Shadmi on 15/05/2023.
+//
+
+import XCTest
+
+final class ImageSearchUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
